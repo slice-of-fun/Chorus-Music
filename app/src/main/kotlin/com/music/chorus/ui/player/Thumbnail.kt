2bcc0269dbcd9cc7c760f7342d4f9087e7cf6f7e
pushkarverse <pushkarkumarsingh.18@gmail.com>
2026-08-31 00:24:06 +0530

    Add missing aspectRatio import in Thumbnail.kt

--- app/src/main/kotlin/com/music/chorus/ui/player/Thumbnail.kt
+++ app/src/main/kotlin/com/music/chorus/ui/player/Thumbnail.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.basicMarquee
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.gestures.snapping.rememberSnapFlingBehavior
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
