d3db4c5a8236b761b7671c04f85c9cd961a10033
pushkarverse <pushkarkumarsingh.18@gmail.com>
2026-09-18 12:28:38 +0530

    ver

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,7 +35,7 @@ android {
         applicationId = "pushkar.chorus.music"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
+        versionCode = 4
         versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
